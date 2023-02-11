13a1abedb20986a48e5e9e2a3efe86e29848cee7
melvin <duchaufour.melvin.sio@gmail.com>
2023-02-11 03:09:37 +0100

    Login Form

--- Login form/js/script.js
+++ Login form/js/script.js
@@ -1,20 +1,15 @@
-// We get the input password and the eye icon
+// Select the input password and the eye icon
 const password = document.querySelector('#password');
 const togglePassword = document.querySelector('#togglePassword');
 
-window.onload=function() {
-    // We call the function when clicked
-    togglePassword.addEventListener('click', function () {
-        // If the input is of type password (*****)
-        if (password.getAttribute('type') == 'password') {
-            // We set the type to text (abcdef)
-            password.setAttribute('type', 'text');
-            // We change the logo (fa-eye-slash when hide) to fa-eye when visible
-            togglePassword.classList.toggle('fa-eye');
-            togglePassword.classList.toggle('fa-eye-slash');
-        } else {
-            password.setAttribute('type', 'password');
-        }
-    });
-}
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute (password -> hide, text -> show)
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('fa-eye');
+    togglePassword.classList.toggle('fa-eye-slash');
+});
+
+
 
